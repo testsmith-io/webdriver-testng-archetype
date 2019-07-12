76e94776aa2375c5ff86b4372fcd0d595c7cce17
Roy de Kleijn <dekleijn.roy@gmail.com>
2019-07-12 13:19:47 +0300

    use loadable component the right way

--- src/main/resources/archetype-resources/src/main/java/pages/AbstractPage.java
+++ src/main/resources/archetype-resources/src/main/java/pages/AbstractPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.LoadableComponent;
 
-abstract class AbstractPage extends LoadableComponent {
+abstract class AbstractPage<T extends LoadableComponent<T>> extends LoadableComponent<T>
     protected WebDriver driver;
 
     public AbstractPage(WebDriver driver) {
