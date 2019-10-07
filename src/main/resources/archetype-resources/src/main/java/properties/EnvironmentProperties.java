5ad242897e5903244c6ab0f8c7bc0a582dc501e8
Roy de Kleijn <dekleijn.roy@gmail.com>
2019-10-07 20:29:23 +0200

    damn, folder in the wrong root

---
+++ src/main/resources/archetype-resources/src/main/java/properties/EnvironmentProperties.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.selenium.properties;
+
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.Sources;
+
+@Sources("file:src/main/resources/${env}_environment.properties")
+public interface EnvironmentProperties extends Config {
+    String url();
+
+    String username();
+
+    String password();
+}
